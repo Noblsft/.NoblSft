3d4188c0390d4a6a957984b05d03c5be8186a4e3
Davide Iacovone <106555044+DavidIacovone@users.noreply.github.com>
2025-12-19 17:47:20 +0100

    Added basic start screen

--- src/pages/desktop/Start.tsx
+++ src/pages/desktop/Start.tsx
@@ -1,3 +1,16 @@
+import {VStack, Button, Image } from "@chakra-ui/react";
+import logo from "../../assets/logo.png";
+
 export default function Start() {
-    return <div>Start Page</div>;
+    return (
+        <VStack>
+            <Image src={logo} alt="logo" aspectRatio={4 / 3} width="350px" />
+            <Button colorScheme="teal" size="lg" width="30%">
+                Create new nobl file
+            </Button>
+            <Button colorScheme="blue" size="lg" width="30%">
+                Load existing nobl file
+            </Button>
+        </VStack>
+    );
 }
